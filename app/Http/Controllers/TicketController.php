a4d0c03576fc499c5da7fd753aabfc09e6ae7179
Mariat28 <mariatndagire28@gmail.com>
2021-06-07 17:18:58 +0300

    added badges and notifications

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -101,12 +101,10 @@ class TicketController extends Controller
         $closedtickets = Ticket::where('status_id', 1)->get();
         $tickets = Ticket::where('status_id', null)->get();
         $departments = Group::where('company_id', Auth::user()->company_id)->get();
-<<<<<<< HEAD
         $prioritylist=TicketPriority::all();
         $opentickets = Ticket::where('status_id', 3)->get();
         $pendingtickets = Ticket::where('status_id', 2)->get();
         return view('adminandsupervisor.closedtickets', compact('closedtickets', 'opentickets','pendingtickets','tickets', 'departments','prioritylist'));
-=======
         $departmentTickets = [];
 
         foreach($departments as $department){
@@ -125,7 +123,6 @@ class TicketController extends Controller
             }
         }
         return view('adminandsupervisor.closedtickets', compact('closedtickets', 'tickets', 'departments', 'departmentTickets'));
->>>>>>> 19a05b03a457d3fee43dff08a0fe991c39944d6a
     }
 
 
@@ -135,12 +132,10 @@ class TicketController extends Controller
         $pendingtickets = Ticket::where('status_id', 2)->get();
         $tickets = Ticket::where('status_id', null)->get();
         $departments = Group::where('company_id', Auth::user()->company_id)->get();
-<<<<<<< HEAD
         $prioritylist=TicketPriority::all();
         $opentickets = Ticket::where('status_id', 3)->get();
         $closedtickets = Ticket::where('status_id', 1)->get();
         return view('adminandsupervisor.pendingtickets', compact('tickets','opentickets','closedtickets', 'pendingtickets', 'prioritylist','departments'));
-=======
         $departmentTickets = [];
 
         foreach($departments as $department){
@@ -159,7 +154,6 @@ class TicketController extends Controller
             }
         }
         return view('adminandsupervisor.pendingtickets', compact('tickets', 'newtickets', 'departments', 'departmentTickets'));
->>>>>>> 19a05b03a457d3fee43dff08a0fe991c39944d6a
     }
 
 
